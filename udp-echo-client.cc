6793130face01fbc9388cf123239aa155c9dfa38
Tailp <33935517+Tailp@users.noreply.github.com>
2018-04-19 17:39:35 +0200

    Add files via upload

--- udp-echo-client.cc
+++ udp-echo-client.cc
@@ -1,20 +1,25 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
-* Copyright 2007 University of Washington
-* 
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software Foundation;
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+ * Copyright 2007 University of Washington
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
 #include "ns3/log.h"
 #include "ns3/ipv4-address.h"
 #include "ns3/ipv6-address.h"
@@ -28,7 +33,7 @@
 #include "ns3/uinteger.h"
 #include "ns3/trace-source-accessor.h"
 #include "udp-echo-client.h"
-
+#include <exception>
 namespace ns3 {
 
 NS_LOG_COMPONENT_DEFINE ("UdpEchoClientApplication");
@@ -93,292 +98,319 @@ UdpEchoClient::~UdpEchoClient()
   m_data = 0;
   m_dataSize = 0;
 }
+int UdpEchoClient::TestConnection(){
+  NodeContainer nodes;
+  nodes.Create (1);
+  //Ipv4AddressHelper address;
+  //address.SetBase ("127.1.1.0", "255.255.255.0");
+  //UdpEchoClientHelper echoClient (m_peerAddress, m_peerport);
+  //echoClient.SetAttribute ("MaxPackets", UintegerValue (nPackets));
+  //echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  //echoClient.SetAttribute ("PacketSize", UintegerValue (2048));
+
+  TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+  Ptr<Socket> testsocket = Socket::CreateSocket (nodes.Get(0), tid); 
+  //ApplicationContainer clientApps = echoClient.Install (nodes.Get (0));
+  //clientApps.Start (Seconds (2.0));
+  //clientApps.Stop (Seconds (3.0));
+  int value = testsocket->Connect (InetSocketAddress (Ipv4Address::ConvertFrom(m_peerAddress), m_peerPort));
+  testsocket->Close();
+  return value;
+}
 
 void 
 UdpEchoClient::SetRemote (Address ip, uint16_t port)
 {
- NS_LOG_FUNCTION (this << ip << port);
- m_peerAddress = ip;
- m_peerPort = port;
+  NS_LOG_FUNCTION (this << ip << port);
+  m_peerAddress = ip;
+  m_peerPort = port;
 }
 
 void 
 UdpEchoClient::SetRemote (Address addr)
 {
- NS_LOG_FUNCTION (this << addr);
- m_peerAddress = addr;
+  NS_LOG_FUNCTION (this << addr);
+  m_peerAddress = addr;
 }
 
 void
 UdpEchoClient::DoDispose (void)
 {
- NS_LOG_FUNCTION (this);
- Application::DoDispose ();
+  NS_LOG_FUNCTION (this);
+  Application::DoDispose ();
 }
 
 void 
 UdpEchoClient::StartApplication (void)
 {
- NS_LOG_FUNCTION (this);
+  NS_LOG_FUNCTION (this);
 
- if (m_socket == 0)
-   {
-     TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
-     m_socket = Socket::CreateSocket (GetNode (), tid);
-     if (Ipv4Address::IsMatchingType(m_peerAddress) == true)
-       {
-         if (m_socket->Bind () == -1)
-           {
-             NS_FATAL_ERROR ("Failed to bind socket");
-           }
-         m_socket->Connect (InetSocketAddress (Ipv4Address::ConvertFrom(m_peerAddress), m_peerPort));
-       }
-     else if (Ipv6Address::IsMatchingType(m_peerAddress) == true)
-       {
-         if (m_socket->Bind6 () == -1)
-           {
-             NS_FATAL_ERROR ("Failed to bind socket");
-           }
-         m_socket->Connect (Inet6SocketAddress (Ipv6Address::ConvertFrom(m_peerAddress), m_peerPort));
-       }
-     else if (InetSocketAddress::IsMatchingType (m_peerAddress) == true)
-       {
-         if (m_socket->Bind () == -1)
-           {
-             NS_FATAL_ERROR ("Failed to bind socket");
-           }
-         m_socket->Connect (m_peerAddress);
-       }
-     else if (Inet6SocketAddress::IsMatchingType (m_peerAddress) == true)
-       {
-         if (m_socket->Bind6 () == -1)
-           {
-             NS_FATAL_ERROR ("Failed to bind socket");
-           }
-         m_socket->Connect (m_peerAddress);
-       }
-     else
-       {
-         NS_ASSERT_MSG (false, "Incompatible address type: " << m_peerAddress);
-       }
-   }
+  if (m_socket == 0)
+    {
+      TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
+      m_socket = Socket::CreateSocket (GetNode (), tid);
+      if (Ipv4Address::IsMatchingType(m_peerAddress) == true)
+        {
+          if (m_socket->Bind () == -1)
+            {
+              NS_FATAL_ERROR ("Failed to bind socket");
+            }
+          m_socket->Connect (InetSocketAddress (Ipv4Address::ConvertFrom(m_peerAddress), m_peerPort));
+        }
+      else if (Ipv6Address::IsMatchingType(m_peerAddress) == true)
+        {
+          if (m_socket->Bind6 () == -1)
+            {
+              NS_FATAL_ERROR ("Failed to bind socket");
+            }
+          m_socket->Connect (Inet6SocketAddress (Ipv6Address::ConvertFrom(m_peerAddress), m_peerPort));
+        }
+      else if (InetSocketAddress::IsMatchingType (m_peerAddress) == true)
+        {
+          if (m_socket->Bind () == -1)
+            {
+              NS_FATAL_ERROR ("Failed to bind socket");
+            }
+          m_socket->Connect (m_peerAddress);
+        }
+      else if (Inet6SocketAddress::IsMatchingType (m_peerAddress) == true)
+        {
+          if (m_socket->Bind6 () == -1)
+            {
+              NS_FATAL_ERROR ("Failed to bind socket");
+            }
+          m_socket->Connect (m_peerAddress);
+        }
+      else
+        {
+          NS_ASSERT_MSG (false, "Incompatible address type: " << m_peerAddress);
+        }
+    }
 
- m_socket->SetRecvCallback (MakeCallback (&UdpEchoClient::HandleRead, this));
- m_socket->SetAllowBroadcast (true);
- ScheduleTransmit (Seconds (0.));
+  m_socket->SetRecvCallback (MakeCallback (&UdpEchoClient::HandleRead, this));
+  m_socket->SetAllowBroadcast (true);
+  ScheduleTransmit (Seconds (0.));
 }
 
 void 
 UdpEchoClient::StopApplication ()
 {
- NS_LOG_FUNCTION (this);
+  NS_LOG_FUNCTION (this);
 
- if (m_socket != 0) 
-   {
-     m_socket->Close ();
-     m_socket->SetRecvCallback (MakeNullCallback<void, Ptr<Socket> > ());
-     m_socket = 0;
-   }
+  if (m_socket != 0) 
+    {
+      m_socket->Close ();
+      m_socket->SetRecvCallback (MakeNullCallback<void, Ptr<Socket> > ());
+      m_socket = 0;
+    }
 
- Simulator::Cancel (m_sendEvent);
+  Simulator::Cancel (m_sendEvent);
 }
 
 void 
 UdpEchoClient::SetDataSize (uint32_t dataSize)
 {
- NS_LOG_FUNCTION (this << dataSize);
+  NS_LOG_FUNCTION (this << dataSize);
 
- //
- // If the client is setting the echo packet data size this way, we infer
- // that she doesn't care about the contents of the packet at all, so 
- // neither will we.
- //
- delete [] m_data;
- m_data = 0;
- m_dataSize = 0;
- m_size = dataSize;
+  //
+  // If the client is setting the echo packet data size this way, we infer
+  // that she doesn't care about the contents of the packet at all, so 
+  // neither will we.
+  //
+  delete [] m_data;
+  m_data = 0;
+  m_dataSize = 0;
+  m_size = dataSize;
 }
 
 uint32_t 
 UdpEchoClient::GetDataSize (void) const
 {
- NS_LOG_FUNCTION (this);
- return m_size;
+  NS_LOG_FUNCTION (this);
+  return m_size;
 }
 
 void 
 UdpEchoClient::SetFill (std::string fill)
 {
- NS_LOG_FUNCTION (this << fill);
+  NS_LOG_FUNCTION (this << fill);
 
- uint32_t dataSize = fill.size () + 1;
+  uint32_t dataSize = fill.size () + 1;
 
- if (dataSize != m_dataSize)
-   {
-     delete [] m_data;
-     m_data = new uint8_t [dataSize];
-     m_dataSize = dataSize;
-   }
+  if (dataSize != m_dataSize)
+    {
+      delete [] m_data;
+      m_data = new uint8_t [dataSize];
+      m_dataSize = dataSize;
+    }
 
- memcpy (m_data, fill.c_str (), dataSize);
+  memcpy (m_data, fill.c_str (), dataSize);
 
- //
- // Overwrite packet size attribute.
- //
- m_size = dataSize;
+  //
+  // Overwrite packet size attribute.
+  //
+  m_size = dataSize;
 }
 
 void 
 UdpEchoClient::SetFill (uint8_t fill, uint32_t dataSize)
 {
- NS_LOG_FUNCTION (this << fill << dataSize);
- if (dataSize != m_dataSize)
-   {
-     delete [] m_data;
-     m_data = new uint8_t [dataSize];
-     m_dataSize = dataSize;
-   }
+  NS_LOG_FUNCTION (this << fill << dataSize);
+  if (dataSize != m_dataSize)
+    {
+      delete [] m_data;
+      m_data = new uint8_t [dataSize];
+      m_dataSize = dataSize;
+    }
 
- memset (m_data, fill, dataSize);
+  memset (m_data, fill, dataSize);
 
- //
- // Overwrite packet size attribute.
- //
- m_size = dataSize;
+  //
+  // Overwrite packet size attribute.
+  //
+  m_size = dataSize;
 }
 
 void 
 UdpEchoClient::SetFill (uint8_t *fill, uint32_t fillSize, uint32_t dataSize)
 {
- NS_LOG_FUNCTION (this << fill << fillSize << dataSize);
- if (dataSize != m_dataSize)
-   {
-     delete [] m_data;
-     m_data = new uint8_t [dataSize];
-     m_dataSize = dataSize;
-   }
+  NS_LOG_FUNCTION (this << fill << fillSize << dataSize);
+  if (dataSize != m_dataSize)
+    {
+      delete [] m_data;
+      m_data = new uint8_t [dataSize];
+      m_dataSize = dataSize;
+    }
 
- if (fillSize >= dataSize)
-   {
-     memcpy (m_data, fill, dataSize);
-     m_size = dataSize;
-     return;
-   }
+  if (fillSize >= dataSize)
+    {
+      memcpy (m_data, fill, dataSize);
+      m_size = dataSize;
+      return;
+    }
 
- //
- // Do all but the final fill.
- //
- uint32_t filled = 0;
- while (filled + fillSize < dataSize)
-   {
-     memcpy (&m_data[filled], fill, fillSize);
-     filled += fillSize;
-   }
+  //
+  // Do all but the final fill.
+  //
+  uint32_t filled = 0;
+  while (filled + fillSize < dataSize)
+    {
+      memcpy (&m_data[filled], fill, fillSize);
+      filled += fillSize;
+    }
 
- //
- // Last fill may be partial
- //
- memcpy (&m_data[filled], fill, dataSize - filled);
+  //
+  // Last fill may be partial
+  //
+  memcpy (&m_data[filled], fill, dataSize - filled);
 
- //
- // Overwrite packet size attribute.
- //
- m_size = dataSize;
+  //
+  // Overwrite packet size attribute.
+  //
+  m_size = dataSize;
 }
 
 void 
 UdpEchoClient::ScheduleTransmit (Time dt)
 {
- NS_LOG_FUNCTION (this << dt);
- m_sendEvent = Simulator::Schedule (dt, &UdpEchoClient::Send, this);
+  NS_LOG_FUNCTION (this << dt);
+  m_sendEvent = Simulator::Schedule (dt, &UdpEchoClient::Send, this);
 }
 
 void 
 UdpEchoClient::Send (void)
-{
- NS_LOG_FUNCTION (this);
+{ 
+  NS_LOG_FUNCTION (this);
 
- NS_ASSERT (m_sendEvent.IsExpired ());
+  NS_ASSERT (m_sendEvent.IsExpired ());
 
- Ptr<Packet> p;
- if (m_dataSize)
-   {
-     //
-     // If m_dataSize is non-zero, we have a data buffer of the same size that we
-     // are expected to copy and send.  This state of affairs is created if one of
-     // the Fill functions is called.  In this case, m_size must have been set
-     // to agree with m_dataSize
-     //
-     NS_ASSERT_MSG (m_dataSize == m_size, "UdpEchoClient::Send(): m_size and m_dataSize inconsistent");
-     NS_ASSERT_MSG (m_data, "UdpEchoClient::Send(): m_dataSize but no m_data");
-     p = Create<Packet> (m_data, m_dataSize);
-   }
- else
-   {
-     //
-     // If m_dataSize is zero, the client has indicated that it doesn't care
-     // about the data itself either by specifying the data size by setting
-     // the corresponding attribute or by not calling a SetFill function.  In
-     // this case, we don't worry about it either.  But we do allow m_size
-     // to have a value different from the (zero) m_dataSize.
-     //
-     p = Create<Packet> (m_size);
-   }
- // call to the trace sinks before the packet is actually sent,
- // so that tags added to the packet can be sent as well
- m_txTrace (p);
- m_socket->Send (p);
+  Ptr<Packet> p;
+  if (m_dataSize)
+    {
+      //
+      // If m_dataSize is non-zero, we have a data buffer of the same size that we
+      // are expected to copy and send.  This state of affairs is created if one of
+      // the Fill functions is called.  In this case, m_size must have been set
+      // to agree with m_dataSize
+      //
+      NS_ASSERT_MSG (m_dataSize == m_size, "UdpEchoClient::Send(): m_size and m_dataSize inconsistent");
+      NS_ASSERT_MSG (m_data, "UdpEchoClient::Send(): m_dataSize but no m_data");
+      p = Create<Packet> (m_data, m_dataSize);
+    }
+  else
+    {
+      //
+      // If m_dataSize is zero, the client has indicated that it doesn't care
+      // about the data itself either by specifying the data size by setting
+      // the corresponding attribute or by not calling a SetFill function.  In
+      // this case, we don't worry about it either.  But we do allow m_size
+      // to have a value different from the (zero) m_dataSize.
+      //
+      p = Create<Packet> (m_size);
+    }
+  // call to the trace sinks before the packet is actually sent,
+  // so that tags added to the packet can be sent as well
+  m_txTrace (p);
+  //if(TestConnection()==(-1)){
+  //  std::cout << "Connection Failed! destination socket missing" << "\n";
+  //}else
+  //{
+  //  std::cout << "Connection successful!" << "\n";
+  //}
+   
+  m_socket->Send (p);
+  
+  
+  ++m_sent;
 
- ++m_sent;
+  if (Ipv4Address::IsMatchingType (m_peerAddress))
+    {
+      NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
+                   Ipv4Address::ConvertFrom (m_peerAddress) << " port " << m_peerPort);
+    }
+  else if (Ipv6Address::IsMatchingType (m_peerAddress))
+    {
+      NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
+                   Ipv6Address::ConvertFrom (m_peerAddress) << " port " << m_peerPort);
+    }
+  else if (InetSocketAddress::IsMatchingType (m_peerAddress))
+    {
+      NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
+                   InetSocketAddress::ConvertFrom (m_peerAddress).GetIpv4 () << " port " << InetSocketAddress::ConvertFrom (m_peerAddress).GetPort ());
+    }
+  else if (Inet6SocketAddress::IsMatchingType (m_peerAddress))
+    {
+      NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
+                   Inet6SocketAddress::ConvertFrom (m_peerAddress).GetIpv6 () << " port " << Inet6SocketAddress::ConvertFrom (m_peerAddress).GetPort ());
+    }
 
- if (Ipv4Address::IsMatchingType (m_peerAddress))
-   {
-     NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
-                  Ipv4Address::ConvertFrom (m_peerAddress) << " port " << m_peerPort);
-   }
- else if (Ipv6Address::IsMatchingType (m_peerAddress))
-   {
-     NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
-                  Ipv6Address::ConvertFrom (m_peerAddress) << " port " << m_peerPort);
-   }
- else if (InetSocketAddress::IsMatchingType (m_peerAddress))
-   {
-     NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
-                  InetSocketAddress::ConvertFrom (m_peerAddress).GetIpv4 () << " port " << InetSocketAddress::ConvertFrom (m_peerAddress).GetPort ());
-   }
- else if (Inet6SocketAddress::IsMatchingType (m_peerAddress))
-   {
-     NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client sent " << m_size << " bytes to " <<
-                  Inet6SocketAddress::ConvertFrom (m_peerAddress).GetIpv6 () << " port " << Inet6SocketAddress::ConvertFrom (m_peerAddress).GetPort ());
-   }
-
- if (m_sent < m_count) 
-   {
-     ScheduleTransmit (m_interval);
-   }
+  if (m_sent < m_count) 
+    {
+      ScheduleTransmit (m_interval);
+    }
 }
 
 void
 UdpEchoClient::HandleRead (Ptr<Socket> socket)
 {
- NS_LOG_FUNCTION (this << socket);
- Ptr<Packet> packet;
- Address from;
- while ((packet = socket->RecvFrom (from)))
-   {
-     if (InetSocketAddress::IsMatchingType (from))
-       {
-         NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client received " << packet->GetSize () << " bytes from " <<
-                      InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
-                      InetSocketAddress::ConvertFrom (from).GetPort ());
-       }
-     else if (Inet6SocketAddress::IsMatchingType (from))
-       {
-         NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client received " << packet->GetSize () << " bytes from " <<
-                      Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
-                      Inet6SocketAddress::ConvertFrom (from).GetPort ());
-       }
-   }
+  NS_LOG_FUNCTION (this << socket);
+  Ptr<Packet> packet;
+  Address from;
+  while ((packet = socket->RecvFrom (from)))
+    {
+      if (InetSocketAddress::IsMatchingType (from))
+        {
+          NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client received " << packet->GetSize () << " bytes from " <<
+                       InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
+                       InetSocketAddress::ConvertFrom (from).GetPort ());
+        }
+      else if (Inet6SocketAddress::IsMatchingType (from))
+        {
+          NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s client received " << packet->GetSize () << " bytes from " <<
+                       Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
+                       Inet6SocketAddress::ConvertFrom (from).GetPort ());
+        }
+    }
 }
 
 } // Namespace ns3
